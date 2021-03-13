f9e8f49c8e987485ba481ea2f9224bf73ae3b502
Botao <wxt2005@gmail.com>
2021-03-13 22:51:53 +0900

    Try disable bootstrap

--- src/App2.js
+++ src/App2.js
@@ -59,7 +59,7 @@ function App() {
           transport: [Websockets, WebRTCStar],
           connEncryption: [NOISE],
           streamMuxer: [Mplex],
-          peerDiscovery: [Bootstrap],
+          // peerDiscovery: [Bootstrap],
           dht: KadDHT
         },
         config: {
@@ -99,17 +99,17 @@ function App() {
             [WebRTCStar.tag]: {
               enabled: true
             },
-            [Bootstrap.tag]: {
-              enabled: true,
-              list: [
-                '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-                '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-                '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
-                '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-                '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-                '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/p2p/QmaPGr8Msp5v669yoWuzGrf2JdQEq3M195DxS3u6u1HNCa'
-              ]
-            }
+            // [Bootstrap.tag]: {
+            //   enabled: true,
+            //   list: [
+            //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+            //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+            //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
+            //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+            //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+            //     '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/p2p/QmaPGr8Msp5v669yoWuzGrf2JdQEq3M195DxS3u6u1HNCa'
+            //   ]
+            // }
           }
         }
       })
